8491ffd756b053c272a67969e6b6236b72225df7
abbas886 <abbas886@yahoo.com>
2017-09-11 10:50:39 +0530

    getMyMessages method - move

--- src/main/java/com/stackroute/ActivityStreamBackend/dao/MessageDao.java
+++ src/main/java/com/stackroute/ActivityStreamBackend/dao/MessageDao.java
@@ -10,6 +10,8 @@ import com.stackroute.ActivityStreamBackend.model.Message;
 public interface MessageDao {
 
 	boolean sendMessage(Message message);
+	
+	//This method should not be in MessageDAO (if UserMessageDAO there) - think about it.
 	List<Message> getMyMessages(String emailId);//create a private method in daoimpl class getmycircleids(email id)
 	//boolean sendMessageToUser(String emailId);
 }
