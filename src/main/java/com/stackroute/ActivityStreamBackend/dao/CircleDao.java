665cb2739f631bc0f42bbce712ff644d007cc47d
abbas886 <abbas886@yahoo.com>
2017-09-11 10:23:58 +0530

    getCircleByUser - discuss

--- src/main/java/com/stackroute/ActivityStreamBackend/dao/CircleDao.java
+++ src/main/java/com/stackroute/ActivityStreamBackend/dao/CircleDao.java
@@ -11,6 +11,12 @@ public interface CircleDao {
 	boolean addCircle(Circle circle);
 	boolean deleteCircle(String emailId,String circleName);
 	List<Circle> getAllCircles();
+	
+	
+	//What you are expecting from this method?  All the Circles of the User?
+	// If yes, you won't get this information from CrcieDAO.
+	//From CircleDAO, you will get all the circles created by a particular user.
+	//Let us discuss..
 	List<Circle> getCircleByUser(String emailId);
 	Circle getCircleByName(String circleName);
 }
